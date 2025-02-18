b0571ac93fccf15f1654aa8e4c6917aa0724758c
DL <duyenlv9@gmail.com>
2025-02-18 17:56:49 +0700

    update configuration

--- LinkAttributionSDK/Classes/Configuration.swift
+++ LinkAttributionSDK/Classes/Configuration.swift
@@ -8,11 +8,11 @@ class ReleaseConfigutation {
     class var SupportedBaseDomains: [String] { ["makeco.ai"] }
 }
 
-#if DEBUG
+//#if DEBUG
 class Configuration: ReleaseConfigutation {
     override class var Server: String { "https://jw4xix6q44.execute-api.us-east-1.amazonaws.com/dev" }
     override class var SupportedBaseDomains: [String] { ["makelabs.ai"] }
 }
-#else
-typealias Configuration = ReleaseConfigutation
-#endif
+//#else
+//typealias Configuration = ReleaseConfigutation
+//#endif
