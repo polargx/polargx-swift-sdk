6f6acbc2300afdd7e550e99719cd3634a7e514ea
DL <duyenlv9@gmail.com>
2025-04-03 14:23:57 +0700

    conttinue update

--- PolarGX-SDK/Classes/Models/DictionaryModel.swift
+++ PolarGX-SDK/Classes/Models/DictionaryModel.swift
@@ -66,11 +66,59 @@ struct DictionaryModel: Codable {
             }else if let value = value as? Encodable {
                 try container.encode(value, forKey: codingKey)
                 
+            }else if let value = value as? Bool {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Int {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Int8 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Int16 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Int32 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Int64 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if #available(iOS 18.0, *), let value = value as? Int128 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? UInt {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? UInt8 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? UInt16 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? UInt32 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? UInt64 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if #available(iOS 18.0, *), let value = value as? UInt128 {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Float {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? Double {
+                try container.encode(value, forKey: codingKey)
+                
+            }else if let value = value as? String {
+                try container.encode(value, forKey: codingKey)
+                
             }else if let value = value as? [String: Any] {
                 try container.encode(DictionaryModel(content: value), forKey: codingKey)
 
             }else {
-                let context = EncodingError.Context(codingPath: container.codingPath, debugDescription: "DictionaryModel.content cannot be encoded")
+                let context = EncodingError.Context(codingPath: container.codingPath, debugDescription: "DictionaryModel.content cannot be encoded: [\(key): \(value) -type \(type(of: value))] ")
                 throw EncodingError.invalidValue(value, context)
             }
         }
