0ddbd8de2cd0156100211c491d003dcec31a6d98
DL <duyenlv9@gmail.com>
2025-09-02 23:38:12 +0700

    UpdateUserModel with clobberMatchingAttributes

--- PolarGX-SDK/Classes/Models/UpdateUserModel.swift
+++ PolarGX-SDK/Classes/Models/UpdateUserModel.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 struct UpdateUserModel: Codable {
+    let clobberMatchingAttributes: Bool
     let organizationUnid: String
     let userID: String
     let data: DictionaryModel
     
     init(organizationUnid: String, userID: String, data: [String: Any]) {
+        self.clobberMatchingAttributes = true //merging enabled
         self.organizationUnid = organizationUnid
         self.userID = userID
         self.data = DictionaryModel(content: data)
