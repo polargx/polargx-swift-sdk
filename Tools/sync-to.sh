7aad186a2267f0c6d51ec5d4b222447cc5614405
DL <duyenlv9@gmail.com>
2025-09-03 15:17:58 +0700

    setup tool for sync, + walk

---
+++ Tools/sync-to.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export PATH="/usr/local/bin:/opt/homebrew/bin:$PATH"
+
+DEST_DIR="$1"
+if [ -n "$DEST_DIR" ]; then
+    echo "DEST_DIR: $DEST_DIR"
+else
+    echo "Error: DEST_DIR is empty or not provided"
+    exit 1
+fi
+
+cd "${SRCROOT}/.." && echo "cd: $(pwd) to replace..." &&
+
+# Copy PolarGX-SDK files
+rm -rf "${DEST_DIR}/polargx-swift-sdk.lib" && mkdir -p "${DEST_DIR}/polargx-swift-sdk.lib" &&
+cp -R ./PolarGX-SDK "${DEST_DIR}/polargx-swift-sdk.lib/" &&
+cp ./PolarGX.podspec "${DEST_DIR}/polargx-swift-sdk.lib/" &&
+cp ./README.md "${DEST_DIR}/polargx-swift-sdk.lib/" &&
+
+cd "${DEST_DIR}" && echo "cd: $(pwd) to pod install..." &&
+pod install
